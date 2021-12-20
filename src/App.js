e38e1e52d0c2ba3fc859a91957f6adf58ce84b00
Mohammad Hossein Khanzadi <mohammad hossein khanzadi>
2021-12-20 13:28:58 +0330

    commit Course3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Button } from '@material-ui/core'
+import { Button, makeStyles } from '@material-ui/core'
 import { Person } from '@material-ui/icons';
+
+const useStyle = makeStyles({
+  Button : {
+    color:"white",
+    backgroundColor: "black"
+  }
+})
 
 
 function App() {
+  const classes = useStyle();
   return (
     <div>
-      <Button variant="outlined" color="secondary" startIcon={<Person/>}>
+      <Button variant="contained" className={classes.Button} size="large" color="secondary" startIcon={<Person/>} >
         سلام
       </Button>
+      <br/>
+
     </div>
     
 
